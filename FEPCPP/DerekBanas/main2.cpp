4c87ac964c9a290da02eff287f9cdf7bab2bb5b6
KaistLecture <keunho@kaist.ac.kr>
2015-07-24 16:01:50 +0900

    add new project

---
+++ FEPCPP/DerekBanas/main2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+// Virtual Methods and Polymorphism
+// Polymorpism allows you to treat subclasses as their superclass and yet
+// call the correct overwritten methods in the subclass automatically
+
+class Animal{
+public:
+	void getFamily() { cout << "We are Animals" << endl; }
+
+	// When we define a method as virtual we know that Animal
+	// will be a base class that may have this method overwritten
+	virtual void getClass() { cout << "I'm an Animal" << endl; }
+};
+
+class Dog : public Animal{
+public:
+	void getClass() { cout << "I'm a Dog" << endl; }
+
+};
+
+class GermanShepard : public Dog{
+public:
+	void getClass() { cout << "I'm a German Shepard" << endl; }
+	void getDerived() { cout << "I'm an Animal and Dog" << endl; }
+
+};
+
+void whatClassAreYou(Animal *animal){
+	animal->getClass();
+}
+
+int main(){
+
+	Animal *animal = new Animal;
+	Dog *dog = new Dog;
+
+	// If a method is marked virtual or not doesn't matter if we call the method
+	// directly from the object
+	animal->getClass();
+	dog->getClass();
+
+	// If getClass is not marked as virtual outside functions won't look for 
+	// overwritten methods in subclasses however
+	whatClassAreYou(animal);
+	whatClassAreYou(dog);
+
+	Dog spot;
+	GermanShepard max;
+
+	// A base class can call derived class methods as long as they exist 
+	// in the base class
+	Animal* ptrDog = &spot;
+	Animal* ptrGShepard = &max;
+
+	// Call the method not overwritten in the super class Animal
+	ptrDog->getFamily();
+
+	// Since getClass was overwritten in Dog call the Dog version
+	ptrDog->getClass();
+
+	// Call to the super class
+	ptrGShepard->getFamily();
+
+	// Call to the overwritten GermanShepard version
+	ptrGShepard->getClass();
+
+	return 0;
+}
---
+++ FEPCPP/DerekBanas/main3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+// Polymorpism allows you to treat subclasses as their superclass and yet
+// call the correct overwritten methods in the subclass automatically
+
+class Animal{
+public:
+	virtual void makeSound(){ cout << "The Animal says grrrr" << endl; }
+
+	// The Animal class could be a capability class that exists
+	// only to be derived from by containing only virtual methods
+	// that do nothing
+
+};
+
+class Cat : public Animal{
+public:
+	void makeSound(){ cout << "The Cat says meow" << endl; }
+
+};
+
+class Dog : public Animal{
+public:
+	void makeSound(){ cout << "The Dog says woof" << endl; }
+
+};
+
+// An abstract data type is a class that acts as the base to other classes
+// They stand out because its methods are initialized with zero
+// A pure virtual method must be overwritten by subclasses
+
+class Car{
+public:
+	virtual int getNumWheels() = 0;
+	virtual int getNumDoors() = 0;
+};
+
+class StationWagon : public Car{
+public:
+	int getNumWheels() { return 4; }
+	int getNumDoors() { return 4; }
+	StationWagon() { }
+	~StationWagon();
+
+};
+
+int main(){
+
+	Animal* pCat = new Cat;
+	Animal* pDog = new Dog;
+
+	pCat->makeSound();
+	pDog->makeSound();
+
+	// Create a StationWagon using the abstract data type Car
+	Car* stationWagon = new StationWagon();
+
+	cout << stationWagon->getNumWheels() << endl;
+
+	return 0;
+}

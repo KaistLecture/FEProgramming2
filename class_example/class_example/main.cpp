a9e2e004375ee2253d08f467212a0bab2e4b759f
fder78 <khhwang78@gmail.com>
2015-11-17 23:45:48 +0900

    lecture 11/17

--- class_example/class_example/main.cpp
+++ class_example/class_example/main.cpp
@@ -20,9 +20,16 @@ public:
 };
 
 std::vector<double> calcRect(Point& p1, Point& p2) {
-	std::vector<double> ret;
+	std::vector<double> ret(2);
+	double x1 = p1.getX();
+	double y1 = p1.getY();
+	double x2 = p2.getX();
+	double y2 = p2.getY();
+	ret[0] = 2 * (std::abs(x1 - x2) + std::abs(y1 - y2));
+	ret[1] = std::abs(x1 - x2) * std::abs(y1 - y2);
 	return ret;
 }
+
 
 int main() {
 	Point A(1, 10);
@@ -30,12 +37,16 @@ int main() {
 	double d1 = A.distance(B);
 	double d2 = B.distance(A);
 	std::vector<double> r = calcRect(A, B);
-	std::cout << d1 << std::endl;
-	std::cout << d2 << std::endl;
 
+	std::cout << "distance from A = " << d1 << std::endl;
+	std::cout << "distance from B = " << d2 << std::endl;
+	std::cout << "perimeter = " << r[0] << std::endl;
+	std::cout << "area = " << r[1] << std::endl;
 }
 
-/*
+
+
+//클래스 예제
 class Stock {
 private:
 	int shares;
@@ -53,7 +64,7 @@ public:
 	}
 };
 
-int main() {
+int main2() {
 	//초기화 생성자
 	Stock a(10, 120);
 	double t = a.get_tot();
@@ -63,17 +74,15 @@ int main() {
 
 
 
-
-
+//함수 예제
 int test(int* n) {
 	return n[0];
 }
 
-
 int f(int a, int b) {return a;}
 int f(int a, int b, int c) { return c; }
 
-int test_main() {
+int main3() {
 	//int a = 10;
 	f(1, 2);
 	{
@@ -85,4 +94,3 @@ int test_main() {
 	std::cout << n << std::endl;
 	return 0;
 }
-*/

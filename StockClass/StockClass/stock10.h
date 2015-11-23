382b253862395be231d5e3aaf975c6aada57409d
KaistLecture <keunho@kaist.ac.kr>
2015-11-23 16:56:19 +0900

    stock example

---
+++ StockClass/StockClass/stock10.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+class Stock
+{
+private:
+	std::string company;
+	long shares;
+	double share_val;
+	double total_val;
+	void set_tot() { total_val = shares * share_val; }
+public:
+	Stock();
+	Stock(const std::string& co, long n = 0, double pr = 0.0);
+	~Stock();
+	void buy(long num, double price);
+	void sell(long num, double price);
+	void update(double price);
+	void show();
+};

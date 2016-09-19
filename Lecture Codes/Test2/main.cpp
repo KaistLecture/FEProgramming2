dd15638bb14654e3247336d8500b746447f6a5f1
KaistLecture <keunho@kaist.ac.kr>
2016-09-19 15:13:35 +0900

    lecture code update

---
+++ Lecture Codes/Test2/main.cpp
@@ -0,0 +1,39 @@
+//Lecture 2
+
+#include <iostream>
+
+int main() {
+	//boolean type
+	
+	//const
+
+	//비교연산자
+
+	//cin
+
+	//function
+
+	//default arguments
+
+	//local global static
+
+	//structure
+
+	//enumerator
+
+	//if else
+
+	//? operator
+
+	//switch case
+
+	//for loop
+
+	//while loop
+
+	//do while loop
+
+	//continue break
+
+	return 0;
+}
---
+++ Lecture Codes/Test3/main.cpp
@@ -0,0 +1,19 @@
+//Lecture 3
+
+#include <iostream>
+
+int main() {
+	//header file
+	
+	//macro
+
+	//reference
+
+	//array
+
+	//vector
+
+	//random normal dist.
+
+	return 0;
+}

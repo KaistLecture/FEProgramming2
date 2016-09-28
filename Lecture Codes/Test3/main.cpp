b8c04126b48615c47dbd80df6c54b3384f1e05f0
KaistLecture <keunho@kaist.ac.kr>
2016-09-28 20:23:42 +0900

    lecture 3

--- Lecture Codes/Test3/main.cpp
+++ Lecture Codes/Test3/main.cpp
@@ -1,11 +1,32 @@
 //Lecture 3
-#define _USE_MATH_DEFINES
 #include <iostream>
-#include <math.h>
+#include "normal.h"
+
+#define XYZ 10 //MACRO º¯¼ö
+#define F(x) (x)*(x)
+
+#ifndef WXYZ
+double foo() {
+	return 10;
+}
+#else
+double foo() {
+	return 20;
+}
+#endif
+typedef double Yield;
 
 int main() {
+	std::cout << foo() << std::endl;
 
 	//&& ||
+	if (1 == 2 && 10 > 5)
+		std::cout << "True" << std::endl;
+	else
+		std::cout << F(1+3) << std::endl;
+
+	Yield p = normcdf(0);
+	std::cout << p << std::endl;
 
 	//header file
 	

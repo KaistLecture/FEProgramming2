593305d42bb7783ba8cf8f722e2c12f6e05d0df1
fder78 <khhwang78@gmail.com>
2016-10-04 21:17:03 +0900

    lecture code: Class

---
+++ Lecture Codes2/OOP_Class/main_class.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main() {
+	
+	//function overloading
+
+	//Class
+	//Member variables, member functions
+	//Constructor & Destructor
+	//public, private, protected
+	
+	return 0;
+}
